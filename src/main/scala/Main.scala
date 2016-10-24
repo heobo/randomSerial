81a602b7b151fe453fed62aa801fc0c9586bdc31
YU <piaoshen@gmail.com>
2016-10-24 16:24:06 +0800

    fix bug

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,7 @@ object Main {
 
   def main(args: Array[String]) {
     val max = 1000
-    val random = new RandomSerial(max, 10, 80)
+    val random = RandomSerial(max, 10, 80)
     val set = 0.until(max).map(random.get).toSet
     println(set.size)
     set.foreach(println)
